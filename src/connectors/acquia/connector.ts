465dd1cc3e6c4b96007f0683c03aeee24769bcca
Pavel Samusev <psamusev@chili-publish.com>
2024-03-20 13:50:34 +0300

    fix(acquia): throw an error for failed requests

--- src/connectors/acquia/connector.ts
+++ src/connectors/acquia/connector.ts
@@ -66,17 +66,10 @@ export default class AcquiaConnector implements Media.MediaConnector {
     const t = await this.runtime.fetch(url, {
       method: 'GET',
     });
-    if (!t?.ok) {
-      this.runtime.logError(
-        `Error while querying Acquia DAM: ${t?.status} - ${t?.statusText}`
+    if (!t.ok) {
+      throw new Error(
+        `Acquia DAM: Detail failed ${t.status} - ${t.statusText}`
       );
-      return {
-        id: '',
-        name: '',
-        relativePath: '',
-        type: 0,
-        metaData: {},
-      };
     }
     const data = JSON.parse(t.text);
     return Converter.assetToMedia(data);
@@ -86,82 +79,62 @@ export default class AcquiaConnector implements Media.MediaConnector {
     options: Connector.QueryOptions,
     context: Connector.Dictionary
   ): Promise<Media.MediaPage> {
-    try {
-      const startIndex = Number(options.pageToken) || 0;
-      const query = context['query'] ?? '';
-      const collection = context['collection'] ?? '';
+    const startIndex = Number(options.pageToken) || 0;
+    const query = context['query'] ?? '';
+    const collection = context['collection'] ?? '';
 
-      // TODO: implement the options.sort and append to query in a proper way
+    // TODO: implement the options.sort and append to query in a proper way
 
-      let url = this.ensureTrailingSlash(this.runtime.options['BASE_URL']);
+    let url = this.ensureTrailingSlash(this.runtime.options['BASE_URL']);
 
-      // mediaId will be used for filtering, so we need to parse it.
-      // filtering could also be just a string, so we need to handle that as well (try/catch)
-      let filter = undefined;
-      if (options.filter && options.filter.length > 0) {
-        try {
-          const temp = JSON.parse(options.filter[0]);
-          filter = temp.eid;
-        } catch (error) {
-          filter = options.filter[0];
-        }
+    // mediaId will be used for filtering, so we need to parse it.
+    // filtering could also be just a string, so we need to handle that as well (try/catch)
+    let filter = undefined;
+    if (options.filter && options.filter.length > 0) {
+      try {
+        const temp = JSON.parse(options.filter[0]);
+        filter = temp.eid;
+      } catch (error) {
+        filter = options.filter[0];
       }
-
-      // We append "collection" filtering if it's provided
-      let finalQuery = collection ? query + ` cn:${collection}` : query;
-      // supporting the queryOptions filter is required for the advanced demo
-      let filterQuery = filter ? ` (eid:${filter} or fn:${filter})` : '';
-      finalQuery = finalQuery + filterQuery;
-
-      url =
-        url +
-        `v2/assets/search?${
-          finalQuery ? 'query=' + finalQuery + '&' : ''
-        }offset=${startIndex * options.pageSize}&limit=${
-          options.pageSize
-        }&expand=thumbnails,metadata,file_properties`;
-
-      const t = await this.runtime.fetch(url, {
-        method: 'GET',
-      });
-
-      if (!t?.ok) {
-        this.runtime.logError(
-          `Error while querying Acquia DAM: ${t?.status} - ${t?.statusText}`
-        );
-        return {
-          pageSize: 0,
-          data: [],
-          links: {
-            nextPage: '',
-          },
-        };
-      }
-
-      const data: GetAssetsResponse = JSON.parse(t.text);
-
-      // transform the data to the MediaPage format
-      const result = {
-        pageSize: options.pageSize,
-        data: data.items.map(Converter.assetToMedia),
-        links: {
-          nextPage: `${
-            data.items.length < options.pageSize ? '' : startIndex + 1
-          }`,
-        },
-      };
-
-      return result;
-    } catch (error) {
-      this.runtime.logError(error);
-      return {
-        pageSize: 0,
-        data: [],
-        links: {
-          nextPage: 'ERROR:: ' + JSON.stringify(error),
-        },
-      };
     }
+
+    // We append "collection" filtering if it's provided
+    let finalQuery = collection ? query + ` cn:${collection}` : query;
+    // supporting the queryOptions filter is required for the advanced demo
+    let filterQuery = filter ? ` (eid:${filter} or fn:${filter})` : '';
+    finalQuery = finalQuery + filterQuery;
+
+    url =
+      url +
+      `v2/assets/search?${
+        finalQuery ? 'query=' + finalQuery + '&' : ''
+      }offset=${startIndex * options.pageSize}&limit=${
+        options.pageSize
+      }&expand=thumbnails,metadata,file_properties`;
+
+    const t = await this.runtime.fetch(url, {
+      method: 'GET',
+    });
+
+    if (!t.ok) {
+      throw new Error(`Acquia DAM: Query failed ${t.status} - ${t.statusText}`);
+    }
+
+    const data: GetAssetsResponse = JSON.parse(t.text);
+
+    // transform the data to the MediaPage format
+    const result = {
+      pageSize: options.pageSize,
+      data: data.items.map(Converter.assetToMedia),
+      links: {
+        nextPage: `${
+          data.items.length < options.pageSize ? '' : startIndex + 1
+        }`,
+      },
+    };
+
+    return result;
   }
   async download(
     id: string,
@@ -169,27 +142,20 @@ export default class AcquiaConnector implements Media.MediaConnector {
     intent: Media.DownloadIntent,
     context: Connector.Dictionary
   ): Promise<Connector.ArrayBufferPointer> {
-    try {
-      const { thumbnails } = JSON.parse(id) as Pick<
-        AcquiaAssetV2,
-        'thumbnails'
-      >;
-      // TODO: Quality should be taken based on "previewType" argument value
-      let thumbnail = thumbnails['600px'];
+    const { thumbnails } = JSON.parse(id) as Pick<AcquiaAssetV2, 'thumbnails'>;
+    // TODO: Quality should be taken based on "previewType" argument value
+    let thumbnail = thumbnails['600px'];
 
-      if (!thumbnail) {
-        // take the last property of thumbnails for given asset
-        const keys = Object.keys(thumbnails);
-        thumbnail = thumbnails[keys[keys.length - 1]];
-      }
-
-      const result = await this.runtime.fetch(thumbnail.url, {
-        method: 'GET',
-      });
-      return result.arrayBuffer;
-    } catch (error) {
-      this.runtime.logError(error);
+    if (!thumbnail) {
+      // take the last property of thumbnails for given asset
+      const keys = Object.keys(thumbnails);
+      thumbnail = thumbnails[keys[keys.length - 1]];
     }
+
+    const result = await this.runtime.fetch(thumbnail.url, {
+      method: 'GET',
+    });
+    return result.arrayBuffer;
   }
   getConfigurationOptions(): Connector.ConnectorConfigValue[] | null {
     return [
