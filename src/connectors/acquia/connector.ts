4561902d55769a1ccb8b264b14c0dc3f618f5231
Abdelhalim Khouas <abdelhalimkhouas@gmail.com>
2024-08-28 14:28:05 +0300

    test change 3

--- src/connectors/acquia/connector.ts
+++ src/connectors/acquia/connector.ts
@@ -1,5 +1,5 @@
 import { Connector, Media } from '@chili-publish/studio-connectors';
-// force change to test build 2
+// force change to test build 3
 interface AcquiaAssetV2 {
   id: string;
   filename: string;
